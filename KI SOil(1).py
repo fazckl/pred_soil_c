68cd99cf324313fd19edcee8c6ee8be3af110dfc
Apsylem <Apsylem@users.noreply.github.com>
2018-11-10 23:32:51 +0100

    Update KI SOil(1).py

--- KI SOil(1).py
+++ KI SOil(1).py
@@ -47,18 +47,18 @@ df = pd.DataFrame(ReadCsv)
 #df = df.iloc[1:,0:]
 
 
-# In[54]:
+# In[109]:
 
 
 #scaleable df
 #scdf = df[['max','median','min','sd_stdDev','aspect','elevation','hillshade','slope','B9','B10']]
-scdf = df[['max','median','min','sd_stdDev','aspect','elevation']]
+scdf = np.array(df[['max','median','min','sd_stdDev','aspect','elevation']])
 #Wholescale needed on this DF
 #nWscdf = df[['B1','B2','B3','B4','B5','B6','B7','B8','B11','B12']]
-nWscdf = df[['B1','B2','B3','B4','B5','B6','B7','B8','B11','B12']]
+nWscdf = df[['B2','B3','B4','B5','B6','B7','B8']]
 
 
-# In[55]:
+# In[110]:
 
 
 max=nWscdf.select_dtypes(include=[np.number]).values.max()
@@ -67,24 +67,24 @@ range=max-min
 range
 
 
-# In[56]:
+# In[111]:
 
 
 #MinMaxscaling
 
 #Wholescaled DF
-Wscdf=nWscdf.sub(min).divide(range)
-
+#Wscdf=nWscdf.sub(min).divide(range)
+Wscdf=scaler.fit_transform(Wscdf)
 #Scaling of Independend DF
 rnames=list(scdf.index)
 cnames=list(scdf)
 scaler = MinMaxScaler()
-scaler.fit(scdf)
-scaled=scaler.transform(scdf)
+
+scaled=scaler.fit_transform(scdf)
 
 
 
-# In[57]:
+# In[112]:
 
 
 rnames=scdf.index
@@ -95,31 +95,31 @@ scaled=pd.concat([scaled, Wscdf], axis=1,ignore_index=True)
 scaled
 
 
-# In[58]:
+# In[113]:
 
 
-skal_vectoren = np.array(scaled.iloc[0:,0:])
+skal_vectoren = scaled#np.array(scaled.iloc[0:,0:])
 C_vectoren = np.array(scaler.fit_transform(df[['C']]))
 
 
-# In[59]:
+# In[114]:
 
 
 C_vectoren
 
 
-# In[65]:
+# In[115]:
 
 
 model = Sequential([
 					Dense(20, input_shape=(skal_vectoren.shape[1],), activation='relu'),
-					Dense(100, activation='relu'),
-					Dense(30, activation='relu'),
-					Dense(1, activation='relu')
+					Dense(1000, activation='relu'),
+					Dense(600, activation='relu'),
+					Dense(1, activation='sigmoid')
 				   ])
 
 				   
-model.compile(Adam(lr=.0001), loss='mean_squared_error', metrics=['accuracy'])
+model.compile(Adam(lr=.1), loss='mean_squared_error', metrics=['accuracy'])
 
 model.fit(skal_vectoren,C_vectoren, epochs=10, batch_size=6, validation_split=10,)
 
